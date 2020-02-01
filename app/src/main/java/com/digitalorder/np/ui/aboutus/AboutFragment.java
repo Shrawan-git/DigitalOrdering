0d5e5c09f15e1738197f7e2071ae5c25d07ffa2f
shrawan <shrawanshaky3@gmail.com>
2020-02-01 14:39:48 +0545

    url set

--- app/src/main/java/com/digitalorder/np/ui/aboutus/AboutFragment.java
+++ app/src/main/java/com/digitalorder/np/ui/aboutus/AboutFragment.java
@@ -1,11 +1,13 @@
 package com.digitalorder.np.ui.aboutus;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
@@ -17,6 +19,7 @@ import com.digitalorder.np.R;
 
 public class AboutFragment extends Fragment {
     private LinearLayout btnlocation;
+    private ImageView twitter, facebook, gmail, insta;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -24,10 +27,50 @@ public class AboutFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_aboutus, container, false);
 
         btnlocation = root.findViewById(R.id.btnlocation);
+        twitter = root.findViewById(R.id.twitter);
+        facebook = root.findViewById(R.id.facebook);
+        gmail = root.findViewById(R.id.gmail);
+        insta = root.findViewById(R.id.insta);
         btnlocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getActivity(), CurrentLocation.class);
+                startActivity(intent);
+            }
+        });
+
+        twitter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.twitter.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        facebook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.facebook.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        gmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.gmail.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(intent);
+            }
+        });
+
+        insta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("http://www.instagram.com");
+                Intent intent = new Intent(Intent.ACTION_VIEW,uri);
                 startActivity(intent);
             }
         });
