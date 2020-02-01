6d47f79034a1a6017ae240341a6fb402884052b8
shrawan <shrawanshaky3@gmail.com>
2020-02-01 14:06:50 +0545

    working on about us

--- app/src/main/java/com/digitalorder/np/ui/aboutus/AboutFragment.java
+++ app/src/main/java/com/digitalorder/np/ui/aboutus/AboutFragment.java
@@ -1,23 +1,36 @@
 package com.digitalorder.np.ui.aboutus;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.digitalorder.np.CurrentLocation;
 import com.digitalorder.np.R;
 
 public class AboutFragment extends Fragment {
+    private LinearLayout btnlocation;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
 
         View root = inflater.inflate(R.layout.fragment_aboutus, container, false);
 
+        btnlocation = root.findViewById(R.id.btnlocation);
+        btnlocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), CurrentLocation.class);
+                startActivity(intent);
+            }
+        });
         return root;
 
     }
